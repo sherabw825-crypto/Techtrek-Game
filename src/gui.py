943ee28977ce0c6975151c5687623ad0db02a0e7
sherabw825-crypto <sherabw825@gmail.com>
2026-07-06 16:58:28 +0600

    code for the gui of game it will be referenced on hollow knight
    logo code was removed becuz the image logo is not going to be utlized any more

--- src/gui.py
+++ src/gui.py
@@ -10,16 +10,13 @@ SCREEN_TITLE = "False Face"
 BACKGROUND_PATH = (
     Path(__file__).resolve().parent.parent / "assets" / "images" / "GUI.jpg"
 )
-LOGO_PATH = (Path(__file__).resolve().parent.parent / "assets" / "images" / "logo.png"
-)
-
 
 class TechtrekWindow(arcade.Window):
     def __init__(self) -> None:
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, fullscreen=True)
         arcade.set_background_color((22, 22, 30))
         self.background = arcade.load_texture(str(BACKGROUND_PATH))
-        self.logo = arcade.load_texture(str(LOGO_PATH))
+
 
     def on_resize(self, width: float, height: float) -> None:
         # This fixes the stretching issue by resetting the viewport to the new window size
@@ -32,12 +29,6 @@ class TechtrekWindow(arcade.Window):
         arcade.draw_texture_rect(
             self.background,
             LBWH(0, 0, self.width, self.height),
-        )
-        
-        # Draws your larger logo in the top left area
-        arcade.draw_texture_rect(
-            self.logo,
-            LBWH(35, self.height - 180 - 45, 380, 180),
         )
 
 
